6a319b75c8adb88cf4527f460069f44e7b7c583c
Divyansh Gandhi <dg1199@gmail.com>
2020-07-31 22:23:16 +0530

    UI Overhaul

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 const DOGO_URL = "https://dog.ceo/api/breeds/image/random"
 
-const container = document.querySelector('.container');
-const button = document.querySelector('.btn')
+const container = document.querySelector('.container')
+const nextBtn = document.querySelector('#next')
+const prevBtn = document.querySelector('#previous')
 const img = document.querySelector('.dogo-img')
+const imageList = [];
 
-button.addEventListener("click", showNewDoggo);
+nextBtn.addEventListener("click", showNewDoggo);
 
-function showNewDoggo(){
-    console.log(' clicked ')
+function showNewDoggo() {
+    console.log('clicked')
     const promise = fetch(DOGO_URL);
     promise
-        .then(function(response) {
-        const processingPromise = response.json();
-        return processingPromise;
+        .then(function (response) {
+            const processingPromise = response.json();
+            return processingPromise;
         })
-        .then(function(processedResponse) {
-        //const img = document.createElement("img");
-        img.src = processedResponse.message;
-        img.alt = "Cute doggo";
-        //container.appendChild(img);
+        .then(function (processedResponse) {
+            imageList.push(processedResponse.message);
+            console.log(imageList)
+            img.src = processedResponse.message;
+            img.alt = "Cute doggo";
         });
 }
 
